2045a01147c53151bc3bc716985ff18271561772
Henrique Sebastião <contato@henriquesebastiao.com>
2025-12-09 05:08:56 -0400

    Add Storm-Breaker

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -3,3 +3,5 @@
 comm -12 <(apt-mark showmanual | sort) <(apt list --installed 2>/dev/null | cut -d/ -f1 | sort) > apt-packages.txt
 
 npm ls -g > npm-packages.txt
+
+/bin/ls -o -g ~/apps/ > other-apps.txt
